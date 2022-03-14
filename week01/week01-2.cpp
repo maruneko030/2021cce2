4588062e34bf1cb9e1e157e0c02fae40661ab45f
maruneko030 <100120550+maruneko030@users.noreply.github.com>
2022-03-14 14:53:24 +0800

    Add files via upload

---
+++ week01/week01-2.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    /*int a;
+    printf("使用者輸入一個值:");
+    scanf("%d",&a);
+
+    printf("你讀到了 %d\n",a);*/
+    char c;
+    printf("請輸入一些字: ");
+    scanf("%c",&c);
+    printf("你讀到了%c\n",c);
+
+    return 0;
+}
