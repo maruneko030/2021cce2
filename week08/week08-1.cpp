5226508a11caa7b0a4999d1d2181b8568d4aa542
maruneko030 <amysu0420@gmail.com>
2022-04-14 20:07:11 +0800

    add week08

---
+++ week08/week08-1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+char names[20][20];
+int main()
+{
+    int grade;
+    for(int i=0;i<3;i++){
+        scanf("%d",&grade);
+    }
+
+    for(int i=0;i<3;i++){
+        printf("d\n",grade);
+    }
+}
